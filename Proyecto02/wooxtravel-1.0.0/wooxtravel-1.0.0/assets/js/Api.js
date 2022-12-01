9c034ba49afc50eece157ed3faef5330d84223cd
Jhi4n <giancarlo845212@gmail.com>
2022-12-01 00:29:05 -0500

    bashboard terminado

--- Proyecto02/wooxtravel-1.0.0/wooxtravel-1.0.0/assets/js/Api.js
+++ Proyecto02/wooxtravel-1.0.0/wooxtravel-1.0.0/assets/js/Api.js
@@ -4,16 +4,48 @@ let cargarDatosApi = () =>{
     .then(data =>{
         data = JSON.parse(data)
         namecountry = Array.from(document.getElementsByClassName("ciudadbanner"));
-        console.log(namecountry[1]);
         namecountry[0].innerHTML = data.networks[0].location.city;
         namecountry[1].innerHTML = data.networks[1].location.city;
         namecountry[2].innerHTML = data.networks[2].location.city;
         namecountry[3].innerHTML = data.networks[3].location.city;
+        for(networks of data.networks){
+            let ciudad = networks.location.city;
+            let plantilla =
+            `
+            <tr>
+                <th onclick="seleccionar(this)">${ciudad}</th>
+            </tr>   
+            `
+            arra.push(networks.name)
+            document.querySelector(".contenedordedatos .tabladedatos .api").innerHTML += plantilla
+        }
+        console.log(arra);
+        namecountry = Array.from(document.getElementsByClassName("encabezado"));
     })
 }
+
+function seleccionar(tr){
+    $(function(){
+        console.log(tr.innerHTML)
+        var texto = tr.innerHTML
+        document.getElementById("buscador").value = texto;
+    })
+}
+function iniciarMap(latitud, longitud){
+    var coords = {lat:latitud ,lng:longitud };
+    var map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 13,
+        center: coords
+    });
+    var marker = new google.maps.Marker({
+        position: coords,
+        map: map
+    });
+}
+
 let arra = []
 
-function obtenerNoticias(){
+function obtenerPais(){
     fetch("http://api.citybik.es/v2/networks")
     .then(response => response.text())
     .then(data =>{
@@ -23,13 +55,12 @@ function obtenerNoticias(){
         }
     if(arra.includes(document.getElementById("buscador").value)){
         const index = arra.indexOf(document.getElementById("buscador").value)
-        console.log(index)
         fetch("http://api.citybik.es/v2/networks")
         .then(response => response.text())
         .then(data =>{
             data = JSON.parse(data)
-                console.log(data["networks"][index])
-                const direccion = data["networks"][index]
+                const direccion = data["networks"][index - 614]
+                console.log(direccion.location.city)
                 const ciudad = document.getElementById("Ciudad")
                 ciudad.innerHTML = direccion.location.city
                 const pais = document.getElementById("Pais")
@@ -38,6 +69,7 @@ function obtenerNoticias(){
                 Latitud.innerHTML = direccion.location.latitude
                 const Longitud = document.getElementById("longitud")
                 Longitud.innerHTML = direccion.location.longitude
+                iniciarMap(direccion.location.latitude, direccion.location.longitude);
             })
     }
     })
